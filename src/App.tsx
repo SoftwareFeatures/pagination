738ada7534f208abe6f7b1dd267b2579ce77deec
chandrakant <chandrakantkarole001995@gmail.com>
2025-03-27 18:34:10 +0530

    FEAT: morden pagination functionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,12 @@ function App() {
   const [productItem, setProductItem] = useState<{ [key: string]: any }[]>([]);
   const [pageNumbers, setPageNumbers] = useState<number[]>([]);
 
+  const limitPerPage = 5;
+  const totalPage = FakeData.length / limitPerPage;
+  const numOfPageShown = 5;
+  const defaultCount = 2;
+
   function setInitialData() {
-    const limitPerPage = 5;
     const start = 0 + ((currPage - 1) * limitPerPage);
     const end = start + limitPerPage;
     const limitedItems = FakeData.slice(start, end);
@@ -18,15 +22,14 @@ function App() {
   useEffect(() => {
     // this fn must be called inside api call
     setInitialData();
-    handlePageNumbers();
+    if (currPage <= totalPage - defaultCount) {
+      handlePageNumbers();
+    }
   }, [currPage])
 
   function handlePageNumbers() {
-    const limitPerPage = 5;
-    const totalPage = FakeData.length / limitPerPage;
-    const numOfPageShown = 3;
     const arr = Array(totalPage).fill("").slice(0, numOfPageShown).map((_, i) => {
-      return currPage > (numOfPageShown) && currPage <= totalPage ? (i + 1) + ((currPage) - (numOfPageShown)) : i + 1
+      return currPage > (defaultCount + 1) ? ((i + 1) + (currPage - (defaultCount + 1))) : (i + 1);
     });
     setPageNumbers(arr);
   }
@@ -53,12 +56,15 @@ function App() {
               <button className='pagination__button' disabled={currPage === 1} onClick={() => currPage !== 1 && setCurrPage(a => a - 1)}>prev</button>
               {
                 pageNumbers.map((count, i) => (
-                  <span key={count} className={`pagination__step ${(currPage > 1 && currPage < 6) && ((i + 1) === Math.ceil(3 / 2)) ? "pagination__step--active" : currPage === count ? "pagination__step--active" : ""}`}>
+                  <span key={count} className={`pagination__step 
+                    ${(currPage > defaultCount && currPage < (totalPage - defaultCount)) && ((i + 1) === Math.ceil(numOfPageShown / 2)) ? "pagination__step--active" : currPage === count ? "pagination__step--active" : ""}
+                  `}>
                     {count}
                   </span>
                 ))
               }
-              <button className='pagination__button' disabled={currPage === (FakeData.length / 5)} onClick={() => currPage !== (FakeData.length / 5) && setCurrPage(a => a + 1)}>next</button>
+              <button className='pagination__button' disabled={currPage === totalPage} 
+              onClick={() => currPage !== totalPage && setCurrPage(a => a + 1)}>next</button>
             </div>
           )
         }
